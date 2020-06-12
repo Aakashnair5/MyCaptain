589801d9f7c00f7660678e8caf5ba4e1f104219e
Aakash P <aakashsatish30@gmail.com>
2020-06-12 23:20:08 +0530

    Created AssigningAccessDelete.py
    Assigns Deletes And element Accessing  from the list,tuple and dictionary.

---
+++ AssigningAccessDelete.py
@@ -0,0 +1,19 @@
+# assigning elements to list 
+list1=[]
+list1.append("aakash")
+print(list1)
+list1.insert(0,"kash")
+print(list1)
+print(list1[1])
+
+
+#accessing elements from tuple
+tuple1=("aakash","kash","hsakaa")
+print(tuple1[1])
+
+
+#deleting different dictionary elements
+dict1={"a":"apple","c":"kayish" , "p":"wow"}
+print(dict1)
+del dict1["c"]
+print(dict1)
